c610a8109b8e7d0e8fb56b078c6f6daa959fc5eb
isaaguilar <isa@aguilarstory.com>
2023-09-02 01:09:20 -0400

    complete swig proto

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,114 @@
-use std::any::Any;
-use std::error;
-use std::error::Error;
+use std::env::consts::OS;
+use std::io::{Error, ErrorKind};
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 use std::path::Path;
-use std::string;
-use std::thread;
+use std::{thread, os, env};
 use std::sync::mpsc::channel;
 use std::sync::mpsc::sync_channel;
 use std::time::Duration;
 use std::slice::Iter;
 use std::str::Chars;
 // use regex::Regex;
+use std::collections::HashMap;
+
+
 
 use notify::Event;
 use notify::event::MetadataKind;
 use notify::{Watcher, RecommendedWatcher, RecursiveMode};
 
 
-fn main() -> std::io::Result<()> {
+#[derive(Debug)]
+struct Log {
+    pub filepath: String,
+    pub uuid: String,
+    pub content: String
+}
 
-    // API handler will first try the request and authenticate only when 401 is returned.
+impl Log {
+    fn new(filepath: &str, uuid: &str) -> std::io::Result<Log> {
+        let mut f = File::open(filepath)?;
+        let mut buffer = String::new();
+        f.read_to_string(&mut buffer)?;
+        Ok(Log{
+            filepath: filepath.to_string(),
+            uuid: uuid.to_string(),
+            content: buffer
+        })
+    }
+}
 
-    // Setup notifiers
-    // Request logs from API for this generation
-    // -> after logs are fetched once, do not fetch them again
-    // -> Read the length of each log item
-    // -> Push logs appending only from byte
-    // After initial push, when notify finds changes to file, append from byte only and push entire file
+#[derive(Debug)]
+struct FileHandler {
+    log: Log,
+}
 
+impl FileHandler {
+    fn new(filepath: &str) -> std::io::Result<FileHandler> {
+        let filename= base(filepath);
+        let filename_parts: Vec<&str> = filename.split(".").collect();
+        if filename_parts.len() != 4 {
+            Err(Error::new(ErrorKind::Other, "Not correct number of dots"))
+            // Err(Error())
+        } else {
+            // let task_name = filename_parts[0];
+            let rerun_id = filename_parts[1];
+            let uuid = filename_parts[2];
 
-    let f = File::open("go.mod")?;
-    let mut reader = BufReader::new(f);
-    let mut line = String::new();
-    let len = reader.read_line(&mut line)?;
-    println!("First line is {len} bytes long");
+            let is_rerun_id_an_int = rerun_id.parse::<u32>().is_ok();
+            let is_uuid = is_valid_uuid(uuid);
 
+            if !is_rerun_id_an_int {
+                Err(Error::new(ErrorKind::Other, "Wrong rerun_id format"))
+            } else if !is_uuid {
+                Err(Error::new(ErrorKind::Other, "Wrong uuid format"))
+            } else {
+                let log = Log::new(filepath, uuid)?;
+                Ok(FileHandler{log})
+            }
+        }
+    }
 
+    fn conent(&self) -> &str {
+        &self.log.content
+    }
 
-    let f = File::open("log1.txt")?;
-    let mut reader = BufReader::new(f);
-    let len_of_log1_txt = reader.read_to_string(&mut line)?;
-    println!("log1.txt is {len_of_log1_txt} bytes long");
+    fn uuid(&self) -> &str {
+        &self.log.uuid
+    }
 
-    let f = File::open("log2.txt")?;
-    let mut reader = BufReader::new(f);
-    let len_of_log2_txt = reader.read_to_string(&mut line)?;
-    println!("log2.txt before log1.txt len is taken into account is {len_of_log2_txt} bytes long");
+    fn filepath(&self) -> &str {
+        &self.log.filepath
+    }
 
-    let f = File::open("log2.txt")?;
-    let mut reader = BufReader::new(f);
-    reader.seek_relative(len_of_log1_txt.try_into().unwrap()).expect("the hell!");
-    let entirelen = reader.read_to_string(&mut line)?;
-    println!("log2.txt after log1.txt len is taken into account is {entirelen} bytes long");
+    #[tokio::main]
+    async fn upload(&self, url: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let mut map = HashMap::new();
+        map.insert("content", self.conent());
+        map.insert("uuid", self.uuid());
 
+        let client = reqwest::Client::new();
+        let res = client.post(url)
+            .json(&map)
+            .send()
+            .await?
+            .text()
+            .await?;
 
-    setup_notifier();
-    ch();
-
-    Ok(())
-    // Ok(())
+        // let resp = reqwest::blocking::get("")?
+        //     .json::<HashMap<String, String>>()?;
+        println!("{}", res);
+        Ok(())
+    }
 
 
 }
 
-fn touch_file() {
 
-}
-
-fn read_on_event(event: Event) {
-    if !is_write_event(&event) {
-        return
-    }
-    if event.paths.len() != 1 {
-        return
-    }
-
-    let filepath = event.paths[0].to_str().unwrap();
-    if !filepath.ends_with(".out") {
-        return
-    }
-    let dirname = dir(filepath);
-    let filename= base(filepath);
-    let name = parse_name(&filename);
-
-    if name.is_ok(){
-        println!("event {:?}", event);
-        let n = name.unwrap();
-        println!("task_name={}, rerun_id={}, dir={}", n.task_name, n.rerun_id, dirname);
-    }
-}
-
-
-
+/// Check if is a write event. Ignore events like metadata changes.
 fn is_write_event(event: &Event) -> bool {
-    // let kind = event.kind;
     if let notify::EventKind::Modify(modify_kind) = event.kind {
         match modify_kind {
             notify::event::ModifyKind::Data(_) => true,
@@ -107,62 +120,15 @@ fn is_write_event(event: &Event) -> bool {
     }
 }
 
-fn is_log_file(filepath: &str) -> bool {
-    filepath.ends_with(".out")
-}
 
-#[derive(Debug)]
-struct LogHandler {
-    task_name: String,
-    rerun_id: String,
-    uuid: String
-}
-
-
+/// Extract the filename from a filepath ie the last item after the last '/'
 fn base(filepath: &str) -> String {
     let items: Vec<&str> = filepath.split("/").collect();
     items.last().unwrap().to_string()
 }
 
-fn dir(filepath: &str) -> String {
-    let mut items: Vec<&str> = filepath.split("/").collect();
-    items.pop();
-    items.join("/")
-}
 
-fn has_three_dots(filename: &str) -> bool {
-    let items: Vec<&str> = filename.split(".").collect();
-    return items.len() == 4
-}
-
-
-
-fn parse_name(filename: &str) -> Result<LogHandler, &str> {
-    let filename_parts: Vec<&str> = filename.split(".").collect();
-    if filename_parts.len() != 4 {
-        Err("Not correct number of dots")
-    } else {
-        let task_name = filename_parts[0];
-        let rerun_id = filename_parts[1];
-        let uuid = filename_parts[2];
-
-        let is_rerun_id_an_int = rerun_id.parse::<u32>().is_ok();
-        let is_uuid = is_valid_uuid(uuid);
-
-        if !is_rerun_id_an_int {
-            Err("Wrong rerun_id format")
-        } else if !is_uuid {
-            Err("Wrong uuid format")
-        } else {
-            Ok(LogHandler{
-                task_name: task_name.to_string(),
-                rerun_id: rerun_id.to_string(),
-                uuid: uuid.to_string()
-            })
-        }
-    }
-}
-
+/// Check if a &str is a uuid
 fn is_valid_uuid(uuid: &str) -> bool {
     // example: ad7e47a1-15a1-44f8-b4cb-924b02e1cc89
     if uuid.len() != 36 {
@@ -173,7 +139,7 @@ fn is_valid_uuid(uuid: &str) -> bool {
             return false;
         } else {
             for (index, &item) in uuid_items.iter().enumerate() {
-                // UUID format is 8-4-4-4-12
+                // UUID format is 8-4-4-4-12 chars + alphanumeric
                 let mut is_correct_chars =  true;
                 is_correct_chars = match index {
                     0 => item.chars().count() == 8 && is_all_alphanumeric(item.chars()),
@@ -192,6 +158,7 @@ fn is_valid_uuid(uuid: &str) -> bool {
     true
 }
 
+/// Check that all chars in an array of chars are alphanumric
 fn is_all_alphanumeric(ch: Chars<'_>) -> bool{
     let b: bool = true;
     for a in ch {
@@ -202,52 +169,62 @@ fn is_all_alphanumeric(ch: Chars<'_>) -> bool{
     b
 }
 
-fn setup_notifier() -> core::result::Result<(), notify::Error> {
+/// For every log event, send a POST request of logs
+fn post_on_event(event: Event, url: &str) {
+    if !is_write_event(&event) {
+        return
+    }
+    if event.paths.len() != 1 {
+        return
+    }
 
-    // Automatically select the best implementation for your platform.
-    let mut watcher = notify::recommended_watcher(|res| {
+    let filepath = event.paths[0].to_str().unwrap();
+    if !filepath.ends_with(".out") {
+        return
+    }
+    let file_handler_result = FileHandler::new(filepath);
+    if file_handler_result.is_err() {
+        println!("File: {}, Error:{:?}", filepath, file_handler_result.unwrap_err());
+        return
+    }
+    let file_handler = file_handler_result.unwrap();
+
+
+    println!("event {:?}", event);
+    println!("File: {}\nuuid: {}", file_handler.filepath(), file_handler.uuid());
+    println!("-CONTENT-\n{}",file_handler.conent());
+
+    file_handler.upload(url);
+
+}
+
+/// Setup and start fs notify.
+/// For each nitification, send a http POST request with full log data.
+fn run_notifier(url: String, logpath: String) -> core::result::Result<(), notify::Error> {
+
+    // Convenience method for creating the RecommendedWatcher for the current platform in immediate mode
+    let mut watcher = notify::recommended_watcher(move |res| {
         match res {
-        Ok(event) => read_on_event(event),
+        Ok(event) => {
+            post_on_event(event, url.as_str())
+        },
         Err(e) => println!("watch error: {:?}", e),
         }
     })?;
 
-    // Watch only the files in the directory
-    watcher.watch(Path::new("logfiles"), RecursiveMode::NonRecursive)?;
-
-
-
-    // Name the files that are in the directory and name when they change
-
-    thread::sleep(Duration::from_secs(600));
-
-
-    Ok(())
+    watcher.watch(Path::new(logpath.as_str()), RecursiveMode::NonRecursive)?;
+    println!("Watching started...");
+    loop{
+        thread::sleep(Duration::from_secs(600));
+    };
 }
 
-fn ch() {
 
-    let (tx, rx) = sync_channel(3);
+fn main() {
+    let url = env::var("TFO_API_LOG_URL").expect("$TFO_API_LOG_URL is not set");
+    let logpath = env::var("LOG_PATH").expect("$LOG_PATH is not set");
 
-    for i in 0..10 {
-        // It would be the same without thread and clone here
-        // since there will still be one `tx` left.
-        let tx = tx.clone();
-        // cloned tx dropped within thread
+    run_notifier(url, logpath).expect("Failed to start notifier");
 
-        let index = i.to_string();
-        thread::spawn(move || tx.send(format!("{index} ok")).unwrap());
-    }
 
-    // Drop the last sender to stop `rx` waiting for message.
-    // The program will not complete if we comment this out.
-    // **All** `tx` needs to be dropped for `rx` to have `Err`.
-    drop(tx);
-
-    // Unbounded receiver waiting for all senders to complete.
-    while let Ok(msg) = rx.recv() {
-        println!("{msg}");
-    }
-
-    println!("completed");
 }
